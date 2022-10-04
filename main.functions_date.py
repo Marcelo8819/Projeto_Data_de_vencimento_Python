7f97f58bca668969f053d2a708481236963debc7
Marcelo8819 <silva.marcelohg@gmail.com>
2022-10-03 22:11:48 -0300

    cirreção com funções de verificações

--- main.functions_date.py
+++ main.functions_date.py
@@ -1,10 +1,13 @@
-from function import today
+from functions import *
 
-print("=============================")
+print("=============================\n")
 print("Qual a data de vencimento? ")
-print("==============================")
+print("Formato sugerido: Dia-Mês-Ano. Exemplo: 01-09-2000\n")
+print("=============================\n")
 
 due_date = input("")
 
-print(today())
-
+if len(due_date) == 10:
+    print(verify_due(due_date))
+else:
+    print("Entrada inválida!")
